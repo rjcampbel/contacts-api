f0ea116c1e611e1c2a132a9b3b4c740bd7d60f59
Ryan Campbell <ryan.campbell4@gmail.com>
2025-01-02 19:33:24 -0600

    Add endpoints for reading contacts

--- server.py
+++ server.py
@@ -2,9 +2,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
+contacts = [
+   { 'id': '1', 'name' : 'Shaun', 'phone': '123-234-3456' },
+   { 'id': '2', 'name' : 'Bob', 'phone': '512-234-3456' },
+   { 'id': '3', 'name' : 'Sue', 'phone': '203-234-3456' },
+   { 'id': '4', 'name' : 'Betty', 'phone': '612-234-3456' },
+]
+
 @app.route('/hello')
 def hello_route():
    print('I have received a request on the /hello endpoint!')
    return '<h1>Hello from the server!</h1>'
 
-app.run(debug=True)
+@app.get('/contacts')
+def list_contacts():
+   return contacts
+
+@app.get('/contacts/<id>')
+def read_single_contact(id):
+   for contact in contacts:
+      if contact['id'] == id:
+         return contact
+
+   return 'That contact does not exist'
+
+if __name__ == '__main__':
+   app.run(port=8000, debug=True)
