cacc87ecf41b1356dc6f132ea4116f592986782a
Ryan Campbell <ryan.campbell4@gmail.com>
2025-01-05 17:58:32 -0600

    Add ability to update and delete contacts

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+next_id = 5
 
 contacts = [
    { 'id': '1', 'name' : 'Shaun', 'phone': '123-234-3456' },
@@ -18,8 +20,37 @@ def read_single_contact(id):
    for contact in contacts:
       if contact['id'] == id:
          return contact
-
    return 'That contact does not exist'
+
+@app.post('/contacts')
+def create_contact():
+   global next_id
+   new_contact = {
+      'id': f'{next_id}',
+      'name': request.json['name'],
+      'phone': request.json['phone']
+   }
+   contacts.append(new_contact)
+   next_id += 1
+
+   return 'Thank you for the data!'
+
+@app.put('/contacts/<id>')
+def update_contact(id):
+   for contact in contacts:
+      if contact['id'] == id:
+         contact['name'] = request.json['name'] if 'name' in request.json else contact['name']
+         contact['phone'] = request.json['phone'] if 'phone' in request.json else contact['phone']
+         return contact
+   return 'That contact does not exist'
+
+@app.delete('/contacts/<id>')
+def delete_contact(id):
+   for contact in contacts:
+      if contact['id'] == id:
+         contacts.remove(contact)
+         return contact
+   return "That contact does not exist"
 
 if __name__ == '__main__':
    app.run(port=8000, debug=True)
